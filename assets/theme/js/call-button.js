3b81ee4f0949557e51e8ef8065698aadc190880e
YulianaShilko <rtrunin@gmail.com>
2026-04-24 07:57:26 +0300

    добавлена кнопка позвонить

---
+++ assets/theme/js/call-button.js
@@ -0,0 +1,18 @@
+(function () {
+    var phoneHref = "tel:+375447707028";
+    var phoneLabel = "+375 44 770-70-28";
+
+    if (document.querySelector(".call-now-button")) {
+        return;
+    }
+
+    var callButton = document.createElement("a");
+    callButton.className = "call-now-button";
+    callButton.href = phoneHref;
+    callButton.setAttribute("aria-label", "Позвонить в Dental Harmony");
+    callButton.innerHTML =
+        '<span class="call-now-button__icon" aria-hidden="true">📞</span><span>Позвонить</span>';
+    callButton.title = phoneLabel;
+
+    document.body.appendChild(callButton);
+})();
